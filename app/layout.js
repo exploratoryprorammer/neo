93658d60ff456f0090437a0df2cbac79ead4e993
Rohan <rohan.k.sah@gmail.com>
2024-08-15 02:32:43 -0500

    analytics update

--- app/layout.js
+++ app/layout.js
@@ -2,19 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -25,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}<Analytics /></body>
     </html>
   );
 }
